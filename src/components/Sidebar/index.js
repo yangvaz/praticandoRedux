8e6e2d70c89868b37f174a7dbd62cbff833e7518
Yan Garcia <yangarciavaz@gmail.com>
2021-02-11 23:37:31 -0300

    practice

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as CourseActions from '../../store/actions/course';
+
+const Sidebar = ({ modules, toggleLesson }) => ( // essa dispatch serve pra disparar actions pro redux
+  <aside>
+    { modules.map(module => (
+      <div key={module.id}>
+        <strong> {module.title} </strong>
+        <ul>
+          {module.lessons.map(lesson => (
+            <li key={lesson.id}>
+              {lesson.title}
+              <button
+                onClick={() => toggleLesson(module, lesson)}
+              >
+                Selecionar
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </aside>
+);
+
+const mapStateToProps = state => ({
+  modules: state.course.modules
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CourseActions, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Sidebar);
